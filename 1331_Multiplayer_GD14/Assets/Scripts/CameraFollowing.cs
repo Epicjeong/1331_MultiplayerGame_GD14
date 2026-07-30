a28b7674c11c94ebf8c4720af73ff0515ae4e7db
Epicjeong <yurijeong@icloud.com>
2026-07-30 02:56:22 -0700

    camera works +background

--- 1331_Multiplayer_GD14/Assets/Scripts/CameraFollowing.cs
+++ 1331_Multiplayer_GD14/Assets/Scripts/CameraFollowing.cs
@@ -5,6 +5,7 @@ public class CameraFollowing : MonoBehaviour
     //refrence to both players
     [SerializeField] private Transform _player1;
     [SerializeField] private Transform _player2;
+    [SerializeField] private float _bounds;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,7 +16,10 @@ public class CameraFollowing : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //makes sure that the camera is always between the players
-        transform.position = new Vector3((_player1.position.x + _player2.position.x) / 2, transform.position.y, transform.position.z);
+        if ((_player1.position.x + _player2.position.x) / 2 <= _bounds && (_player1.position.x + _player2.position.x) / 2 >= -_bounds)
+        {
+            //makes sure that the camera is always between the players
+            transform.position = new Vector3((_player1.position.x + _player2.position.x) / 2, transform.position.y, transform.position.z);
+        }
     }
 }
